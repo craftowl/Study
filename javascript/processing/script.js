d20a6651e4ed20af1b466e44f8acf348e1afcb0e
devowl <devowl@mac>
2023-09-27 23:57:50 +0900

    test source and, p5.js initial

---
+++ javascript/processing/script.js
@@ -0,0 +1,18 @@
+// 初期設定
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    strokeWeight(10)
+    stroke(240)
+    noFill()
+    circle(width / 4, height / 4, 50);
+
+    stroke(100)
+    fill(255)
+    circle(width / 1.5, height / 1.5, 200);
+
+}
+
+function draw() {
+
+}
